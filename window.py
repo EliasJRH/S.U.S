dfa2e2539fd5554e145dae4739017e151864d272
Elias Hawa <elias.hawa2468@gmail.com>
2023-03-05 08:23:38 -0500

    Have program stop when window is closed

--- window.py
+++ window.py
@@ -5,18 +5,18 @@ IMPOSTOR_FILE_PATH = "media/Impostor.png"
 TWERK_FILE_PATH = "media/among-us-twerk.gif"
 
 layout = [ [sg.Text("Smart Ubiquitous Speaker", key='-bold-', justification='center')],[sg.Image(IMPOSTOR_FILE_PATH, key="-image-")] ]
-window = sg.Window('Window Title', layout)
+window = sg.Window('S(mart) U(biquitous) S(peaker)', layout)
 
-def handle_sigusr1():
-  window["-image-"].update(IMPOSTOR_FILE_PATH)
+# def handle_sigusr1():
+#   window["-image-"].update(IMPOSTOR_FILE_PATH)
 
-def handle_sigusr2():
-  window["-image-"].update(TWERK_FILE_PATH)
+# def handle_sigusr2():
+#   window["-image-"].update(TWERK_FILE_PATH)
 
 def main():
-  ppid = str(sys.argv[1])
-  print(f"This process id: {os.getpid()}")
-  print(f"Parent process id: {ppid}")
+  ppid = int(sys.argv[1])
+  # print(f"This process id: {os.getpid()}")
+  # print(f"Parent process id: {ppid}")
 
   sg.theme('DarkAmber')   # Add a touch of color
   # All the stuff inside your window.
@@ -25,6 +25,7 @@ def main():
   while True:
       event, values = window.read()
       if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+          os.kill(ppid, signal.SIGTERM)
           break
       print('You entered ', values[0])
 
